caa670bc956c9c2228749de94f4bef3629eab28a
issamwahbi <84291685+issamwahbi@users.noreply.github.com>
2024-10-27 21:05:42 +0100

    init

--- custom_components/quran_browser/media_source.py
+++ custom_components/quran_browser/media_source.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from typing import Any, Dict, List
 from homeassistant.components.media_player import MediaClass, MediaType
 from homeassistant.components.media_source import (
     BrowseMediaSource,
@@ -11,17 +12,19 @@ from homeassistant.components.media_source import (
     Unresolvable,
 )
 
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 
 from .quran_client import QuranClient
 from . import QuranBrowserConfigEntry
 from .const import DOMAIN
+
 
 async def async_get_media_source(hass: HomeAssistant) -> QuranMediaSource:
     """Set up Quran Browser media source."""
     entry = hass.config_entries.async_entries(DOMAIN)[0]
 
     return QuranMediaSource(hass, entry)
+
 
 class QuranMediaSource(MediaSource):
     """Provide Quran stations as media sources."""
@@ -44,27 +47,53 @@ class QuranMediaSource(MediaSource):
         quranClient = self.quranClient
         reciter_id = self.entry.data["reciter_id"]
 
-        chapter = await quranClient.fetch_chapter(reciter_id=reciter_id, chapter_id=int(item.identifier))
+        chapter = await quranClient.fetch_chapter(
+            reciter_id=reciter_id, chapter_id=int(item.identifier)
+        )
         if not chapter:
             raise Unresolvable("Quran chapter not found")
 
-
         return PlayMedia(chapter.audio_url, "audio/mpeg")
-    
-    async def async_browse_media(
-            self,
-            item: MediaSourceItem,
-        ) -> BrowseMediaSource:
-            """Return media."""
-            radios = self.radios
 
-            return BrowseMediaSource(
-                domain=DOMAIN,
-                identifier=None,
-                media_class=MediaClass.CHANNEL,
-                media_content_type=MediaType.MUSIC,
-                title=self.entry.title,
-                can_play=True,
-                can_expand=True,
+    async def async_browse_media(
+        self,
+        item: MediaSourceItem,
+    ) -> BrowseMediaSource:
+        """Return media."""
+        quranClient = self.quranClient
+        chapters = await quranClient.fetch_chapters()
+
+        return BrowseMediaSource(
+            domain=DOMAIN,
+            identifier=None,
+            media_class=MediaClass.CHANNEL,
+            media_content_type=MediaType.MUSIC,
+            title=self.entry.title,
+            can_play=False,
+            can_expand=True,
+            children=[
+                *await self._async_build_chapters(quranClient, chapters),
+            ],
+        )
+
+    @callback
+    def _async_build_chapters(
+        self, quranClient: QuranClient, chapters: List[Dict[str, Any]]
+    ) -> list[BrowseMediaSource]:
+        """Build list of media sources from radio stations."""
+        items: list[BrowseMediaSource] = []
+
+        for chapter in chapters:
+            items.append(
+                BrowseMediaSource(
+                    domain=DOMAIN,
+                    identifier=chapter.id,
+                    media_class=MediaClass.MUSIC,
+                    media_content_type="MUSIC",
+                    title=chapter.name_simple,
+                    can_play=True,
+                    can_expand=False,
+                )
             )
 
+        return items
